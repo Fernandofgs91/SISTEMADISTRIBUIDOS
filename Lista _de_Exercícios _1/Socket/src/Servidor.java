d5aa2fd406b4d4a6dc049881268a615d7827c4b7
FERNANDO GUIMARÃES SILVA <162770609+Fernandofgs91@users.noreply.github.com>
2025-09-28 20:55:23 -0300

    Update Servidor.java

--- Lista _de_Exercícios _1/Socket/src/Servidor.java
+++ Lista _de_Exercícios _1/Socket/src/Servidor.java
@@ -1,16 +1,31 @@
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Servidor {
-    public static void main(String[] args) throws Exception {
-        try (ServerSocket s = new ServerSocket(50000, 50)) {
-            System.out.println("Servidor iniciado na porta 50000");
+    private static final int PORTA = 12345;
+   
+    private static final Set<ServidorSocketThread> clientes =
+            Collections.synchronizedSet(new HashSet<>());
+
+    public static void main(String[] args) {
+        try (ServerSocket servidor = new ServerSocket(PORTA)) {
+            System.out.println("Servidor iniciado na porta " + PORTA);
+
             while (true) {
-                Socket con = s.accept();
-                System.out.println("Conexão estabelecida de " + con.getRemoteSocketAddress());
-                ServidorSocketThread socketThread = new ServidorSocketThread(con);
-                new Thread(socketThread).start();
+                Socket socket = servidor.accept();
+                System.out.println("Novo cliente conectado: " + socket);
+
+                ServidorSocketThread clienteThread = new ServidorSocketThread(socket, clientes);
+                clientes.add(clienteThread);
+
+                new Thread(clienteThread).start();
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
