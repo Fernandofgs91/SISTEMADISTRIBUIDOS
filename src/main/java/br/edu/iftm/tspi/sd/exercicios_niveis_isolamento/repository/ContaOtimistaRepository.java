aee1bc7273542ce779b5a74639d2bbdf43a840f3
FERNANDO GUIMARÃES SILVA <162770609+Fernandofgs91@users.noreply.github.com>
2025-11-25 16:15:33 -0300

    Delete src directory

--- src/main/java/br/edu/iftm/tspi/sd/exercicios_niveis_isolamento/repository/ContaOtimistaRepository.java
+++
@@ -1,7 +0,0 @@
-package br.edu.iftm.tspi.sd.exercicios_niveis_isolamento.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import br.edu.iftm.tspi.sd.exercicios_niveis_isolamento.domain.ContaOtimista;
-
-public interface ContaOtimistaRepository extends JpaRepository<ContaOtimista, String> {
-}
